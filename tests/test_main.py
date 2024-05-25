74e5826bc16bac4a89cac5b7a8d1bc3a1a9d577c
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-05-25 18:00:24 +0500

    PIAIC root

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,10 @@ def test_root_path():
 
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+def test_piaic_root():
+    client = TestClient(app=app)
+    responce = client.get("/piaic/")
+
+    assert responce.status_code == 200
+    assert responce.json() == {"Organization": "PIAIC"}

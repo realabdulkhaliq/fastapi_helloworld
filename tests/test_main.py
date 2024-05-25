f9dc7db2851c77b9cfa8eca6c9fb0244a9f06c52
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-05-25 17:55:36 +0500

    test_root_path():

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,11 @@
 from fastapi.testclient import TestClient
+from fastapi_helloworld.main import app
+
+
+def test_root_path():
+    client = TestClient(app=app)
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
